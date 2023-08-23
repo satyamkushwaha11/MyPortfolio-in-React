dcc8b85c6ff5fc77bb9727a8a079af3ae66a4dc9
satyamkushwaha11 <satyamkushwaha1101@gmail.com>
2023-08-23 20:17:47 +0530

    issue resolved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/header/Header';
-import CustomCursor from './components/cursor/CustomCursor';
+// import CustomCursor from './components/cursor/CustomCursor';
 import Homepage from './pages/Main/homepage/Homepage';
 
 function App() {
